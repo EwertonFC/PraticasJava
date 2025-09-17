c833b6b947af75c5ea540c71303352e7df4661e4
Ewerton Ferreira <99198148+EwertonFC@users.noreply.github.com>
2025-09-17 17:55:15 -0300

    add: classe Pessoa com método de saudação

---
+++ src/Exercicios1/Pessoa.java
@@ -0,0 +1,8 @@
+package Exercicios1;
+
+public class Pessoa {
+
+    void pessoaCumprimentar () {
+        System.out.println("Olá, mundo!");
+    }
+}
---
+++ src/Exercicios1/Principal.java
@@ -0,0 +1,9 @@
+package Exercicios1;
+
+public class Principal {
+    public static void main(String[] args) {
+        Pessoa pessoa1 = new Pessoa();
+        pessoa1.pessoaCumprimentar();
+    }
+
+}
