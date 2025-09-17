6f37207c45b78eb2e66a4678e3056fd0ef9a6898
Ewerton Ferreira <99198148+EwertonFC@users.noreply.github.com>
2025-09-17 18:17:50 -0300

    add: classe Calculadora com método de dobro

---
+++ src/Exercicios1/Calculadora.java
@@ -0,0 +1,10 @@
+package Exercicios1;
+
+public class Calculadora {
+
+    double retornaDobro (double n1){
+        System.out.println(n1 * 2);
+        return n1 * 2;
+    }
+}
+
--- src/Exercicios1/Principal.java
+++ src/Exercicios1/Principal.java
@@ -4,6 +4,10 @@ public class Principal {
     public static void main(String[] args) {
         Pessoa pessoa1 = new Pessoa();
         pessoa1.pessoaCumprimentar();
+
+        Calculadora calculo1 = new Calculadora();
+        calculo1.retornaDobro(4);
+
     }
 
 }
